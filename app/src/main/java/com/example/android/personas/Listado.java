e6f17b1ad0f09921db89c39575e17dd8d9737cfd
Walderman <walderman@garcia.com>
2017-09-30 09:18:19 -0500

    Interfaces terminadas

---
+++ app/src/main/java/com/example/android/personas/Listado.java
@@ -0,0 +1,13 @@
+package com.example.android.personas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+    }
+}
